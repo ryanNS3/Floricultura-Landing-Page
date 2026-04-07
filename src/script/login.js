49fc8124158c4e059f5a91e66c427d731b34f6e0
Nicolascommits <nicolasrochamartins25@gmail.com>
2026-04-07 17:00:46 -0300

    Validação de login e armazenamento no storage

--- src/script/login.js
+++ src/script/login.js
@@ -13,3 +13,35 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = './index.html';
     });
 });
+
+
+const formLogin = document.querySelector('#formLogin');
+
+if (formLogin) {
+  formLogin.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.querySelector('#emailLogin').value;
+    const senha = document.querySelector('#senhaLogin').value;
+
+    const lista = JSON.parse(localStorage.getItem("cadastros") || "[]");
+
+    // procura usuário
+    const usuarioEncontrado = lista.find(user => 
+      user.email === email && user.senha === senha
+    );
+
+    if (usuarioEncontrado) {
+      alert("Login realizado com sucesso!");
+
+      // opcional: salvar sessão
+      localStorage.setItem("usuarioLogado", JSON.stringify(usuarioEncontrado));
+
+      // redireciona
+      window.location.href = "./index.html";
+
+    } else {
+      alert("E-mail ou senha incorretos!");
+    }
+  });
+}
